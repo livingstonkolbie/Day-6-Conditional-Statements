3578a3eb356a27cf54fb76e359d0628659e616cc
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-04-21 15:29:35 -0600

    updated some problems

--- JS/problems.js
+++ JS/problems.js
@@ -12,6 +12,16 @@ if (number > 0) {
     console.log ("This number is zero");
 }
 
+number = -4;
+
+if(number > 0) {
+    console.log(`${number} is a positive number`);
+} else if(number < 0) {
+    console.log(`${number} is a negative number`);
+} else {
+    console.log(`${number} is zero`);
+}
+
 
 //Problem 2
 console.log ("Problem 2");
@@ -38,13 +48,29 @@ if (score >= 90) {
 // Problem 3 
 console.log("Problem 3");
 
-let age = 155;
+let age = 15;
 
 if (age >= 65) {
-    console.log("Eligible for Senior Citizen Discount");
+    console.log(`Eligible for Senior Citizen Discount`);
 } else {
     console.log("Not eligible for Senior Citizen Discount");
 }
+
+let eligible = age >= 65 ? "Eligible" : "Not eligible";
+console.log(eligible);
+
+
+switch (age >= 65) {
+    case true:
+        console.log("Old");
+        break;
+    case false:
+        console.log("Young");
+        break;
+    default:
+        console.log("Wizard");
+}
+
 
 
 // Problem 4
@@ -52,7 +78,7 @@ console.log("Problem 4");
 
 let hour = 14;
 
-if (hour > 5 && hour < 12) {
+if (hour >= 5 && hour < 12) {
     console.log("Good Morning!");
 } else if (hour >= 12 && hour < 17) {
     console.log ("Good Afternoon!");
@@ -67,14 +93,17 @@ if (hour > 5 && hour < 12) {
 console.log("Problem 5");
 
 age = 74;
+let price;
 
 if (age < 12) {
-    console.log ("Price is $5");
+    price = "5";
 } else if (age >=12 && age <= 64) {
-    console.log ("Price is $12");
+    price = "12";
 } else if (age > 64) {
-    console.log ("Price is $8");
+    price = "8";
 }
+
+console.log(`Price: $${price}`);
 
 
 // Problem 6
@@ -84,13 +113,31 @@ let weightKg = 70;
 let heightM = 1.75;
 
 let bmi = weightKg / (heightM * heightM);
+let category;
 
 if (bmi < 18.5) {
-    console.log ("Underweight");
+    category = "underweight"
 } else if (bmi < 25) {
-    console.log ("Normal Weight");
+    category = "normal weight"
 } else if (bmi < 30) {
-    console.log ("Overweight");
+    category = "overweight"
 } else  {
-    console.log ("Obese");
+    category = "obese"
+}
+
+console.log(`BMI: ${bmi.toFixed(1)} Category: ${category}`);
+
+let weightKg = 710;
+let heightM = 1.75;
+
+let bmi = weightKg / (heightM * heightM);
+
+if (bmi < 18.5) {
+    console.log ("This is under weight");
+} else if (bmi < 25) {
+    console.log ("This is normal weight");
+} else if (bmi < 30) {
+    console.log ("This is over weight");
+} else  {
+    console.log ("This is obese");
 }
